108f2b6c7c9472dac973e4afcdedd930753e84e9
Rodrigo Zani <rodrigo.zhs@gmail.com>
2015-03-21 13:14:48 -0300

    add materialize

--- simple-todos.js
+++ simple-todos.js
@@ -1,23 +1,13 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.body.helpers({
+    tasks : [
+      {text: "This is a task"},
+      {text: "This is other task"},
+      {text: "an another task"},
+    ]
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
