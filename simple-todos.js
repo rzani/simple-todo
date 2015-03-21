8717de140f78ff8f4aed2cbccd618eec8c6b5d68
Rodrigo Zani <rodrigo.zhs@gmail.com>
2015-03-21 15:07:07 -0300

    first funcional version

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,41 @@
+//mongo collection
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  Template.body.helpers({
-    tasks : [
-      {text: "This is a task"},
-      {text: "This is other task"},
-      {text: "an another task"},
-    ]
-  });
+	//finding tasks
+	Template.body.helpers({
+		tasks: function() {
+			return Tasks.find({},{sort: {checked: 1, created: -1}});
+		}
+	});
+
+	//inserting tasks
+	Template.insert.events({
+		'submit .insert': function (event) {
+			//This function is called when the new task form is submitted
+			var text = event.target.text.value;
+
+			Tasks.insert({
+				text: text,
+				created: new Date()  //current time
+			});
+
+			//clear form
+			event.target.text.value = "";
+
+			//prevent submit
+			return false;
+		}
+	});
+
+	Template.task.events({
+		"click .toggle-checked": function(){
+			Tasks.update(this._id, {$set: {checked: ! this.checked}});
+		},
+		"click .delete": function () {
+    	Tasks.remove(this._id);
+  	}
+	});
 }
 
 if (Meteor.isServer) {
